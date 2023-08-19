476e8c2a2e119a1161365b1915499f192d8f2061
Diana Krempels <cem@MacBook-Air-CEM.local>
2023-08-19 11:27:11 +0200

    completed task splice-values

--- scripts/forme.js/splice-values.js
+++ scripts/forme.js/splice-values.js
@@ -4,15 +4,27 @@
  * Пройтись масивом і замінити кожне значення 10 на текст - "ten";
  */
 
+/** numbers.splice(5, 1, "ten");
+*numbers.splice(8, 1, "ten");
+* console.log(numbers);
+* const changedArray = numbers.map((number) => {
+*   if (number === 10) {
+*        return "ten";
+*    } else {
+*       return number
+*   }
+*});
+*console.log(numbers);
+*/
+
 const numbers = [3,4,5,3,2,10,34,24,10,3,5,77,5,23,2,45,6];
-numbers.splice(5, 1, "ten");
-numbers.splice(8, 1, "ten");
-console.log(numbers);
-const changedArray = numbers.map((number) => {
-    if (number === 10) {
-        return "ten";
-    } else {
-        return number
-    }
-});
-console.log(numbers);
+function replaceEveryTenIntoWord(numbers) {
+    return numbers.map((number) => {
+        if (number === 10) {
+            return "ten";
+        } else {
+            return number
+        }
+    });
+}
+console.log(replaceEveryTenIntoWord(numbers));
