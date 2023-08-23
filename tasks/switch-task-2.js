7df8656df7b93abc653d4b6f4b87325ad420a129
Diana Krempels <cem@MacBook-Air-CEM.fritz.box>
2023-08-23 10:01:09 +0200

    new task

---
+++ tasks/switch-task-2.js
@@ -0,0 +1,42 @@
+/**
+ * Є масив об'єктів (доповнити цей масив)
+ * [
+ * {
+ *  user:"",
+ *  birthday:"July"
+ * }, 
+ * {
+ *  user:"",
+ *  birthday:"May"
+ * },
+ * ...
+ * ];
+ * Відфільтрувати так, щоб вивести 4 масиви з тільки user, народжених восени, взимку, навесні та влітку.
+ * Кінцевий результат має бути:
+ * const summaryUsers = ["username1","username2","username3"];
+ * ...
+ */
+
+
+const summaryUsers = [
+{
+user:"Diana",
+birthday:"April"
+},
+{
+    user:"Lev",
+    birthday:"February"
+},
+{
+    user:"Eva",
+    birthday:"June"
+},
+{
+    user:"Vita",
+    birthday:"September"
+}
+];
+
+switch (birthday) {
+    case 
+}
