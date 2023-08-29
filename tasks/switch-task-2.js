6b76a69f9a92b3d2e09c4f36481d7eb29f4ef7d2
Diana Krempels <cem@macbook-air-cem.home>
2023-08-29 22:47:45 +0200

    completed task switch-task

--- tasks/switch-task-2.js
+++ tasks/switch-task-2.js
@@ -37,7 +37,31 @@ const users = [
 }
 ];
 
-const summaryUsers = users.filter(function (el) {
-    return el.birthday === "April";
+const summaryUsers = [[], [], [], []];
+
+users.forEach((user) => {
+  switch (user.birthday) {
+    case "September":
+    case "October":
+    case "November":
+        summaryUsers[0].push(user.user);
+        break;
+    case "December":
+    case "January":
+    case "February":
+      summaryUsers[1].push(user.user);
+      break;
+    case "March":
+    case "April":
+    case "May":
+      summaryUsers[2].push(user.user);
+      break;
+    case "June":
+    case "July":
+    case "August":
+      summaryUsers[3].push(user.user);
+      break;
+  }
 });
+
 console.log(summaryUsers);
