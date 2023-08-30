532dbd19b2cf00b9ce3becc396bb04e6c6123c8a
Diana Krempels <cem@macbook-air-cem.home>
2023-08-30 22:31:55 +0200

    modified file

--- tasks/reduce-task-users.js
+++ tasks/reduce-task-users.js
@@ -16,19 +16,3 @@ const users = [
         birthday:"September"
     }
     ];
-
-    const summaryUsers = users.reduce((acc, curr) => {
-        const season = Object.keys(seasons).find((key) => seasons[key].includes(curr.birthday));
-        return {
-            ...acc,
-            [season]: [
-                ...(acc[season] || []),
-                curr.user
-            ]
-        };
-    }, {});
-    
-    console.log(summaryUsers); // { autumn: [ 'Diana' ], winter: [ 'Lev' ], summer: [ 'Eva' ], spring: [ 'Vita' ] }
-    
-    const finalResult = Object.values(summaryUsers).flat();
-    console.log(finalResult); // [ 'Diana', 'Lev', 'Eva', 'Vita' ]
