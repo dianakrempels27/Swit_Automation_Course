ae38957464719ae308fa0a6ec0bbc7079c32b04c
Diana Krempels <cem@macbook-air-cem.home>
2023-09-05 18:28:03 +0200

    completed task async-await

---
+++ tasks/async-await.js
@@ -0,0 +1,21 @@
+/**
+ * Є функція:
+ * await waitTimer(timer) {
+ *      let i = 0;
+ *      while(timer < i) {
+ *          console.log(`Wait ${i} seconds`);
+ *          i++;
+ *      }
+ * }
+ * Написати вивід функції і протестувати функцію
+ */
+
+async function waitTimer(timer) {
+    let i = 0;
+    while(timer > i) {
+        console.log(`Wait ${i} seconds`);
+        i++;
+    }
+}
+
+waitTimer(2);
