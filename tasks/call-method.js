c5b77cab43a0a22c1ac16444c429a6f2db536668
Diana Krempels <cem@macbook-air-cem.home>
2023-09-05 19:26:02 +0200

    completed view and buy method task

--- tasks/call-method.js
+++ tasks/call-method.js
@@ -12,19 +12,25 @@
  * }
  */
 
-const product = {name:"", id:""};
 const shop = {
     title:"Kids",
     products:["dolls","car","piramida", "box", "sachok"],
-    buy(product) {
-        const newProduct = this.products.splice(newProduct);
-    },
-    view() {
-        const result = `${this.name} product with ${this.id} id`;
+    view(product) {
+        const result = `${product.name} product with ${product.id} id`;
         console.log(result);
         return result;
-    } 
- };
-
- shop.buy("car");
- shop.view();
+    },
+    buy(productName) {
+        const index = this.products.indexOf(productName);
+        if (index > -1) {
+            this.products.splice(index, 1);
+            console.log(`${productName} has been removed from the products list.`);
+            return true;
+        }
+        console.log(`${productName} is not found in the products list.`);
+        return false;
+    }
+ }
+ 
+ const product = {name:"car", id:"100"};
+ shop.buy(product.name);
