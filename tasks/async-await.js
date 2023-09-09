d2441ce1f2575bba5ad0fbcacf9ae504165fa02b
Diana Krempels <cem@macbook-air-cem.home>
2023-09-09 16:26:40 +0200

    new changes

--- tasks/async-await.js
+++ tasks/async-await.js
@@ -14,10 +14,9 @@ async function waitTimer(timer) {
     let i = 0;
     while(timer < i) {
         console.log(`Wait ${i} seconds`);
+        await new Promise(resolve => setTimeout(resolve, 1000));
         i++;
     }
 }
 
-waitTimer(2).then(result => {
-    console.log(result);
-});
+waitTimer(5);
