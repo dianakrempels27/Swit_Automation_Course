0dd5a61dbf60aeceb90f2b2177b1a570a1659acc
Diana Krempels <cem@macbook-air-cem.home>
2023-09-26 11:20:43 +0200

    test create post pass

--- cypress/e2e/mocked/cypress/create.user.profile.test.spec.cy.js
+++ cypress/e2e/mocked/cypress/create.user.profile.test.spec.cy.js
@@ -1,24 +1,21 @@
 describe("Create User Profile Test", () => {
     beforeEach(() => {
         cy.visit("http://stage3.switschooltest.com");
-        cy.login({
-            login:"diana.krempels1",
-            password:"09Tirogo&"
-        })
-    })
+    });
 
     it("should be displayed profile registration form after clicking on Профіль button", () => {
-        cy.visit('.rd-nav-link[href="/profile"]').click();
+        cy.visit(Cypress.config('baseUrl') + '/login')
+        cy.get('input[name=login]').type("diana.krempels1")
+        cy.get('input[name=password').type("09Tirogo&")
+        cy.get('button[type="submit"]').click();
+        cy.get("[href='/profile']").click();
         cy.get("h1").contains("Профіль");
-        cy.get("[placeholder='Ім'я']").type("Diana");
+        cy.get("[name='name']").type("Diana");
         cy.get("[placeholder='Прізвище']").type("Krempels");
+        cy.get("[placeholder='Дата народження']").type("27.04.2000");
         cy.get("[placeholder='Країна']").type("Ukraine");
         cy.get("[placeholder='Нікнейм']").type("Bulka");
-        cy.get("[type='file']").click();
-        cy.get("[type='file']").type("126559-muzyka-biom-svet-nebo-atmosfera-1280x720.jpg");
+        cy.get("[name='about']").type("Trying to become Automation QA!");
         cy.get("[type='submit']").click();
-        cy.get('input').each(($input) => {
-            cy.wrap($input).should('have.value');
-          });
-    })
-})
+    });
+});
