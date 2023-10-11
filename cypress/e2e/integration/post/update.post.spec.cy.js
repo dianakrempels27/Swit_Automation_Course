c8072e866aa7e326849ea30e5d5912b05c6189c1
Diana Krempels <cem@macbook-air-cem.home>
2023-10-11 16:04:20 +0200

    Update post via API test

--- cypress/e2e/integration/post/update.post.spec.cy.js
+++ cypress/e2e/integration/post/update.post.spec.cy.js
@@ -1,4 +1,4 @@
-describe("update post created by API", () => {
+describe("Update post created by API", () => {
     beforeEach(() => {
         cy.loginViaApi();
         cy.createPostViaApi("Test post via API");
@@ -6,10 +6,25 @@ describe("update post created by API", () => {
     });
 
     it("should be updated post after clicking on the Редагувати допис button", () => {
-        cy.get("h4").contains("Test post via API").click();
-        cy.get(".btn.btn-primary").contains("Редагувати допис").click();
-        cy.get("[placeholder='Текст допису']").clear().type("Have a good day :)")
-        cy.get("[type='submit']").contains("Редагувати допис").click();
-        cy.get("[placeholder='Текст допису']").should("have.value", "Have a good day :)");
+        cy.request({
+            method: "PUT",
+            url: "https://unit1105.p-host.kiev.ua/post/65268f3bfd56a263d8f13601",
+            body: {
+                "image": {},
+                "title": "Greetings post",
+                "description": "Helle, user!",
+                "content": "Have a good day :)"
+            },headers: {
+                "Content-Type":"application/json",
+                "Authorization":`Bearer ${Cypress.env("token")}`
+            }
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+            })
+        })
+        //cy.get("h4").contains("Test post via API").click();
+        //cy.get(".btn.btn-primary").contains("Редагувати допис").click();
+        //cy.get("[placeholder='Текст допису']").clear().type("Have a good day :)")
+        //cy.get("[type='submit']").contains("Редагувати допис").click();
+        //cy.get("[placeholder='Текст допису']").should("have.value", "Have a good day :)");
     });
-})
