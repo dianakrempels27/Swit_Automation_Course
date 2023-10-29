74ced1f6370d47d5cd159b2b7167ca59c6373ba6
Diana Krempels <cem@macbook-air-cem.home>
2023-10-29 15:04:38 +0100

    api testing login spec

--- api/specs/login.js
+++ api/specs/login.js
@@ -5,7 +5,7 @@ describe("login API", () => {
     it("should be return status 200 after valid credentials are provided", async () => {
         const login = await usersEndpoints.post("diana.krempels1", "09Tirogo&");
         console.log(login.statusCode);
-        //expect(login.statusCode).to.be.eql(200);
-        //expect(login.body).to.be.eql("diana.krempels1");
+        expect(login.statusCode).to.be.eql(200);
+        expect(login.body).to.be.eql("diana.krempels1");
     });
 });
