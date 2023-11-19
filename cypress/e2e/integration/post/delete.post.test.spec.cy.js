69558d46407cf00d588dff282e91e50f9a32da58
Diana Krempels <cem@MacBook-Air-CEM.fritz.box>
2023-11-19 16:59:06 +0100

    updated delete post test

--- cypress/e2e/integration/post/delete.post.test.spec.cy.js
+++ cypress/e2e/integration/post/delete.post.test.spec.cy.js
@@ -5,7 +5,7 @@ describe("Create Post Test", () => {
     })
 
     it("should be deleted post after clicking on the Видалити допис button", () => {
-        cy.get("h4").contains("My trip to London").click()
+        cy.get("h4").contains("1700047388").click()
         cy.get(".btn.btn-danger").contains("Видалити допис").click()
         cy.get("h4").contains("My trip to London").should("not.exist");
     })
